1ecc52cf47005ae278b679cf150cde169481d677
JONGGON <rlawhdrhs27@gmail.com>
2024-01-23 03:19:37 +0900

    update

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -28,6 +28,57 @@ export const siteMetadata = {
     },
     icons: {
         icon: '/favicon.png',
+        apple: '/favicon.png',
+    },
+    appleWebApp: {
+        title: 'for apple splash',
+        startupImage: [
+            {
+                media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+                url: '/splashscreens/iphone5_splash.png',
+            },
+            {
+                media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+                url: '/splashscreens/iphone5_splash.png',
+            },
+            {
+                media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+                url: '/splashscreens/iphone6_splash.png',
+            },
+            {
+                media: '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+                url: '/splashscreens/iphoneplus_splash.png',
+            },
+            {
+                media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+                url: '/splashscreens/iphonex_splash.png',
+            },
+            {
+                media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+                url: '/splashscreens/iphonexr_splash.png',
+            },
+            {
+                media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+                url: '/splashscreens/iphonexsmax_splash.png',
+            },
+            {
+                media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+                url: '/splashscreens/ipad_splash.png',
+            },
+            {
+                media: '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+                url: '/splashscreens/ipadpro1_splash.png',
+            },
+            {
+                media: '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+                url: '/splashscreens/ipadpro3_splash.png',
+            },
+            {
+                media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+                url: '/splashscreens/ipadpro2_splash.png',
+            },
+        ],
     },
     manifest: '/manifest.webmanifest',
 };
+
